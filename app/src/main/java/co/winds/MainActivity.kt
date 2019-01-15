219845032c862498a68dc52e9ee3050ac708e8b2
karun <karun@winds.co.in>
2019-01-15 16:32:10 +0530

    fcm in app messaging

---
+++ app/src/main/java/co/winds/MainActivity.kt
@@ -0,0 +1,14 @@
+package co.winds
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+   // https://firebase.google.com/docs/in-app-messaging/get-started
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
